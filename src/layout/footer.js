db41a8dab4ef4bb5c49d8fb44b77004a1427c62c
Maojin-juan <edwardelric199887@gmail.com>
2024-07-14 22:42:30 +0800

    首頁：手機版

---
+++ src/layout/footer.js
@@ -0,0 +1,15 @@
+import '../style/style.css'
+
+document.querySelector('#footer').innerHTML = `
+  <nav class="bg-secondary">
+      <div class="container flex justify-between">
+        <img class="logo pt-4 pb-[15.43px]" src="https://github.com/hexschool/2022-web-layout-training/blob/main/week3-4/logo-white.png?raw=true" alt="logo">
+        <ul class=" text-white flex items-center">
+          <li><a class="pl-[64px] py-[25px]" href="#">系列鏡框</a></li>
+          <li><a class="pl-[64px] py-[25px]" href="#">門市據點</a></li>
+          <li><a class="pl-[64px] py-[25px]" href="#">部落格</a></li>
+          <li><a class="pl-[64px] py-[25px]" href="#">常見問題</a></li>
+        </ul>
+      </div>
+    </nav>
+`
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,14 +3,58 @@ export default {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
+      backgroundImage: {
+        'sm-banner':
+          'url("https://github.com/hexschool/2022-web-layout-training/blob/main/week3-4/home-header-sm.png?raw=true")',
+        banner:
+          'url("https://github.com/hexschool/2022-web-layout-training/blob/main/week3-4/home-header.png?raw=true")',
+          
+        'md-glasses':
+          'url("https://github.com/hexschool/2022-web-layout-training/blob/main/week3-4/index-bg-md.png?raw=true")',
+        'sm-glasses':
+          'url("https://github.com/hexschool/2022-web-layout-training/blob/main/week3-4/index-bg-sm.png?raw=true")',
+        glasses:
+          'url("https://github.com/hexschool/2022-web-layout-training/blob/main/week3-4/index-bg.png?raw=true")',
+      },
+
+      fontSize: {
+        '2xl': ['1.5rem',{
+          lineHeight: '2.25rem',
+          fontWeight: '700',
+        }],
+      },
+
       colors: {
-        'primary': '#FBF2FB',
-        'secondary': '#AA0601',
-        'secondary-dark': '#650300'
+        'bgc': '#FCF3F1',
+        'main': '#AA0601',
+        'main-dark': '#650300'
       },
 
       fontFamily: {
-        w3font: ['Roboto', 'Noto Sans TC', 'sans-serif'],
+        sans: ['Noto Sans TC', 'Roboto', 'sans-serif'],
+      },
+      
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: 'var(--tw-gutter-x, 14px)',
+          md: 'var(--tw-gutter-x, 0.75rem)',
+        },
+        screens: {
+          sm: '540px',
+          md: '720px',
+          lg: '960px',
+          xl: '1140px',
+          '2xl': '1320px',
+        },
+      },
+
+      screens: {
+        sm: '576px',
+        md: '768px',
+        lg: '992px',
+        xl: '1200px',
+        '2xl': '1400px',
       },
     },
   },
