50c21d5bd7c359592bf382e1d24532954df0560e
Maojin-juan <edwardelric199887@gmail.com>
2024-07-16 01:25:03 +0800

    navbar、footer移至js，新增home-2頁面

--- src/layout/footer.js
+++ src/layout/footer.js
@@ -1,15 +1,82 @@
-import '../style/style.css'
+import "../style/style.css";
 
-document.querySelector('#footer').innerHTML = `
-  <nav class="bg-secondary">
-      <div class="container flex justify-between">
-        <img class="logo pt-4 pb-[15.43px]" src="https://github.com/hexschool/2022-web-layout-training/blob/main/week3-4/logo-white.png?raw=true" alt="logo">
-        <ul class=" text-white flex items-center">
-          <li><a class="pl-[64px] py-[25px]" href="#">系列鏡框</a></li>
-          <li><a class="pl-[64px] py-[25px]" href="#">門市據點</a></li>
-          <li><a class="pl-[64px] py-[25px]" href="#">部落格</a></li>
-          <li><a class="pl-[64px] py-[25px]" href="#">常見問題</a></li>
-        </ul>
-      </div>
-    </nav>
-`
+document.querySelector("#footer").innerHTML = `
+  <footer class="bg-main py-6 text-white md:pb-10 md:pt-12">
+      <ul class="contact container">
+        <li class="flex flex-wrap items-center justify-between pb-2">
+          <ul class="hidden gap-12 md:flex">
+            <li><a class="btn-hover block py-6" href="./index.html">首頁</a></li>
+            <li><a class="btn-hover block py-6" href="./home-2.html">系列鏡框</a></li>
+            <li><a class="btn-hover block py-6" href="#">門市據點</a></li>
+            <li><a class="btn-hover block py-6" href="#">部落格</a></li>
+            <li><a class="btn-hover block py-6" href="#">常見問題</a></li>
+          </ul>
+          <a class="flex items-center md:hidden" href="tel:+0800-000-000">
+            <span class="material-symbols-outlined mr-2 text-[18px]">
+              call
+            </span>
+            0800-000-000
+          </a>
+          <ul class="flex gap-2 md:gap-4">
+            <li>
+              <a class="btn-hover block size-6 md:size-10 lg:size-12" href="#"
+                ><img
+                  src="https://github.com/hexschool/2022-web-layout-training/blob/main/week3-4/ic-social-fb.png?raw=true"
+                  alt="facebook"
+              /></a>
+            </li>
+            <li>
+              <a class="btn-hover block size-6 md:size-10 lg:size-12" href="#"
+                ><img
+                  class="size-6 md:size-10 lg:size-12"
+                  src="https://github.com/hexschool/2022-web-layout-training/blob/main/week3-4/ic_social_ig.png?raw=true"
+                  alt="instagram"
+              /></a>
+            </li>
+            <li>
+              <a class="btn-hover block size-6 md:size-10 lg:size-12" href="#"
+                ><img
+                  class="size-6 md:size-10 lg:size-12"
+                  src="https://github.com/hexschool/2022-web-layout-training/blob/main/week3-4/ic_social_line.png?raw=true"
+                  alt="line"
+              /></a>
+            </li>
+          </ul>
+        </li>
+        <li class="mx-auto flex flex-col gap-2">
+          <a
+            class="hover hidden items-center self-start md:flex md:text-4xl md:leading-[54px]"
+            href="tel:+0800-000-000"
+          >
+            <span
+              class="material-symbols-outlined mr-4 text-[18px] md:text-5xl"
+            >
+              call
+            </span>
+            0800-000-000
+          </a>
+          <a
+            class="hover flex items-center md:self-start md:text-4xl md:leading-[54px]"
+            href="mailto:glasses@business.com"
+          >
+            <span
+              class="material-symbols-outlined mr-2 text-[18px] md:mr-4 md:text-5xl"
+            >
+              mail
+            </span>
+            glasses@business.com
+          </a>
+        </li>
+        <hr class="mb-4 mt-6 md:my-8" />
+      </ul>
+      <ul
+        class="container flex flex-col gap-2 md:flex-row md:justify-between md:pt-0"
+      >
+        <li>Copyright © 2020 Glasses.All rights reserved.</li>
+        <div class="flex flex-col gap-2 md:flex-row md:gap-12">
+          <li>隱私權政策</li>
+          <li>服務條款</li>
+        </div>
+      </ul>
+    </footer>
+`;
--- src/layout/navbar.js
+++ src/layout/navbar.js
@@ -1,15 +1,46 @@
-import '../style/style.css'
+import "../style/style.css";
 
-document.querySelector('#navbar').innerHTML = `
-  <nav class="bg-secondary">
-      <div class="container flex justify-between">
-        <img class="logo pt-4 pb-[15.43px]" src="https://github.com/hexschool/2022-web-layout-training/blob/main/week3-4/logo-white.png?raw=true" alt="logo">
-        <ul class=" text-white flex items-center">
-          <li><a class="pl-[64px] py-[25px]" href="#">系列鏡框</a></li>
-          <li><a class="pl-[64px] py-[25px]" href="#">門市據點</a></li>
-          <li><a class="pl-[64px] py-[25px]" href="#">部落格</a></li>
-          <li><a class="pl-[64px] py-[25px]" href="#">常見問題</a></li>
+document.querySelector("#navbar").innerHTML = `
+  <nav class="bg-main">
+      <div class="flex flex-col justify-between md:container md:flex-row">
+        <a class="logo ml-[15px] py-4 md:ml-0" href="#">
+          <img
+            src="https://github.com/hexschool/2022-web-layout-training/blob/main/week3-4/logo-white.png?raw=true"
+            alt="logo"
+          />
+        </a>
+        <ul
+          class="flex flex-wrap items-center text-center text-white md:flex-nowrap md:text-xl md:leading-[30px]"
+        >
+          <li class="w-1/2 md:w-auto">
+            <a
+              class="btn-hover block text-nowrap px-[62px] py-3 outline outline-1 outline-white md:px-0 md:pl-16 md:outline-none"
+              href="#"
+              >系列鏡框</a
+            >
+          </li>
+          <li class="w-1/2 md:w-auto">
+            <a
+              class="btn-hover block text-nowrap px-[62px] py-3 outline outline-1 outline-white md:px-0 md:pl-16 md:outline-none"
+              href="#"
+              >門市據點</a
+            >
+          </li>
+          <li class="w-1/2 md:w-auto">
+            <a
+              class="btn-hover block text-nowrap px-[62px] py-3 outline outline-1 outline-white md:px-0 md:pl-16 md:outline-none"
+              href="#"
+              >部落格</a
+            >
+          </li>
+          <li class="w-1/2 md:w-auto">
+            <a
+              class="btn-hover block text-nowrap px-[62px] py-3 outline outline-1 outline-white md:px-0 md:pl-16 md:outline-none"
+              href="#"
+              >常見問題</a
+            >
+          </li>
         </ul>
       </div>
     </nav>
-`
+`;
