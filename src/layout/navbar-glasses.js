50382c94452045685dd8569f2db39ef6c8043a8e
Maojin-juan <edwardelric199887@gmail.com>
2024-07-18 17:01:05 +0800

    新增social svg並完成第八頁，剩餘hover細節待補

--- src/layout/navbar-glasses.js
+++ src/layout/navbar-glasses.js
@@ -6,7 +6,7 @@ document.querySelector("#navbar-glasses").innerHTML = `
       class="flex items-center lg:container md:text-2xl lg:text-[32px] lg:leading-normal"
     >
       <li
-        class="border-b-1 relative flex flex-1 justify-center border border-[#DCDCDC]"
+        class="border-b-1 relative flex-1 border border-[#DCDCDC]"
       >
         <input
           type="radio"
@@ -17,26 +17,26 @@ document.querySelector("#navbar-glasses").innerHTML = `
         />
         <label
           for="tab1"
-          class="checked after:navbar-selected w-full cursor-pointer py-4 text-center font-medium lg:py-6"
-          ><a href="./home-2.html">OPTICAL</a></label
+          class="checked after:navbar-selected"
+          ><a class="py-4 text-center lg:py-6 font-medium block" href="./home-2.html">OPTICAL</a></label
         >
       </li>
       <li
-        class="border-b-1 relative flex flex-1 justify-center border border-[#DCDCDC]"
+        class="border-b-1 relative flex-1 border border-[#DCDCDC]"
       >
         <input type="radio" name="tabs" id="tab2" class="checked hidden" />
         <label
           for="tab2"
-          class="checked after:navbar-selected w-full cursor-pointer py-4 text-center font-medium lg:py-6"
-          ><a href="./home-3.html">SUNGLASSES</a></label
+          class="checked after:navbar-selected"
+          ><a class="py-4 text-center lg:py-6 font-medium block" href="./home-3.html">SUNGLASSES</a></label
         >
       </li>
-      <li class="border-b-1 relative flex flex-1 justify-center border">
+      <li class="border-b-1 relative flex-1 border">
         <input type="radio" name="tabs" id="tab3" class="checked hidden" />
         <label
           for="tab3"
-          class="checked after:navbar-selected w-full cursor-pointer py-4 text-center font-medium lg:py-6"
-          ><a href="./home-4.html">FUNCTIONAL</a></label
+          class="checked after:navbar-selected"
+          ><a class="py-4 text-center lg:py-6 font-medium block" href="./home-4.html">FUNCTIONAL</a></label
         >
       </li>
     </ul>
