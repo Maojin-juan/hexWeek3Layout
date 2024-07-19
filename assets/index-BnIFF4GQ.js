740bd4621c0dde88fc000b96cb30c641e33f8c2c
Maojin-juan <edwardelric199887@gmail.com>
2024-07-19 10:24:40 +0800

    Updates

--- assets/index-BnIFF4GQ.js
+++ assets/index-BnIFF4GQ.js
@@ -16,7 +16,7 @@
         <li class="w-1/2 md:w-auto">
           <a
             class="navbar-list"
-            href="./pages/home-2.js"
+            href="../pages/home-2.js"
           >
             系列鏡框
           </a>
